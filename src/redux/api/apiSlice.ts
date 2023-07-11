c192fecb6f0172202091c2015ad0414de734d2f9
Saif Maamun <sadifmamun@gmail.com>
2023-07-11 17:53:59 +0600

    RTk Query

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,1 +1,16 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:5000/` }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+    singleProduct: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useSingleProductQuery } = api;
