a652ff807df86dca1e9cdc8b5247ad9be5211e5e
Saif Maamun <sadifmamun@gmail.com>
2023-07-13 11:45:19 +0600

    navigate

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -12,11 +12,19 @@ import {
 import { HiOutlineSearch } from 'react-icons/hi';
 import Cart from '../components/Cart';
 import logo from '../assets/images/technet-logo.png';
-import { useAppSelector } from '@/redux/hook';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { signOut } from 'firebase/auth';
+import { auth } from '@/lib/firebase';
+import { setUser } from '@/redux/features/user/userSlice';
 
 export default function Navbar() {
+  const dispatch =useAppDispatch()
 const handleLogout=()=>{
   console.log('handleLogout()')
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    dispatch(setUser(null))
+});
 }
   const {user}= useAppSelector((state)=>state.user)
   return (
